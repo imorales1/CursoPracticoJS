9bed12e3918bd8efa4406db0d7f152506c54f59e
imorales1 <alejan_dro-morales@hotmail.com>
2022-05-22 23:10:15 -0600

    Taller #2 Finalizado

--- Taller#2/Descuentos.js
+++ Taller#2/Descuentos.js
@@ -3,18 +3,26 @@ const descuento = 15;
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioCDescuento = 100 - descuento;
-    const precioConDescuento = (PrecioOriginal * porcentajePrecioCDescuento)/100;
+    const precioConDescuento = (precio * porcentajePrecioCDescuento)/100;
 
     return precioConDescuento;
 }
-
-
 
 console.log("El precio original es," + PrecioOriginal + "");
 console.log({
     PrecioOriginal,
     descuento,
-    porcentajePrecioCDescuento,
-    precioConDescuento,
-});
+    //porcentajePrecioCDescuento,
+    //precioConDescuento,
+}); 
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const inputDiscount = document.getElementById("InputDiscount");
+    const ResultPrice = document.getElementById("ResultPrice");
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+
+    ResultPrice.innerText = "El precio con descuento es Q." + calcularPrecioConDescuento(priceValue, discountValue) + ".00.";
+}
 
